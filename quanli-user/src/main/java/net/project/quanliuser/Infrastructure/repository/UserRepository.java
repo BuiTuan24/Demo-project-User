f4957a815ab34f152e191d9f74bd43ef4604a5cf
BuiTuan24 <162609498+BuiTuan24@users.noreply.github.com>
2024-03-24 16:09:10 +0700

    Delete quanli-user directory

--- quanli-user/src/main/java/net/project/quanliuser/Infrastructure/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package net.project.quanliuser.Infrastructure.repository;
-
-import net.project.quanliuser.domain.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository <UserEntity, String> {
-}
--- quanli-user/src/main/java/net/project/quanliuser/TextGit.java
+++
@@ -1,7 +0,0 @@
-package net.project.quanliuser;
-
-public class TextGit {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
